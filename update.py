a5770cab481dbf98ee366e6fc9f9244e4fd2efd8
savitarf <115222680+savitarf@users.noreply.github.com>
2022-10-08 09:44:04 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,12 +37,12 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/savitarf/Gd-b/"
+    UPSTREAM_REPO = "https://github.com/savitarf/rc2"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
 except:
-    UPSTREAM_BRANCH = 'h-code'
+    UPSTREAM_BRANCH = 'master'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
